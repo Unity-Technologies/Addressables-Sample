7c7e59f725027e4d2c832d8957cfaac62ed152cf
Bill Ramsour <bill@unity3d.com>
2019-04-22 17:38:32 -0500

    sync addressables demo!

---
+++ Advanced/Sync Addressables/Assets/SyncAddressables/SyncBundleProvider.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.ResourceManagement.ResourceProviders;
+
+public class SyncBundleProvider : AssetBundleProvider
+{
+
+    class SyncAssetBundleResource : IAssetBundleResource
+    {
+        AssetBundle m_AssetBundle;
+        public AssetBundle GetAssetBundle()
+        {
+            return m_AssetBundle;
+        }
+
+        internal void Start(ProvideHandle provideHandle)
+        {
+            m_AssetBundle = AssetBundle.LoadFromFile(provideHandle.Location.InternalId);
+            if(m_AssetBundle == null)
+                Debug.LogError("the bundle failed " + provideHandle.Location.InternalId);
+            provideHandle.Complete(this, m_AssetBundle != null, null);
+        }
+    }
+    
+    public override void Provide(ProvideHandle providerInterface)
+    {
+        new SyncAssetBundleResource().Start(providerInterface);
+    }
+}
---
+++ Advanced/Sync Addressables/Assets/spawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+
+    int counter = 0;
+    void FixedUpdate()
+    {
+        if (!SyncAddressables.Ready)
+            return;
+        
+        counter++;
+
+        if (counter == 1)
+        {
+//            var cube = SyncAddressables.LoadAsset<GameObject>("Cube");
+//            if(cube == null)
+//                Debug.LogWarning("null :(");
+//            else
+//                Debug.LogWarning("not null :)");
+
+            var go = SyncAddressables.Instantiate("Cube");
+           
+            go.transform.forward = new Vector3(Random.Range(0,180), Random.Range(0,180), Random.Range(0,180));
+        }
+
+
+        if (counter >= 60)
+            counter = 0;
+    }
+}
