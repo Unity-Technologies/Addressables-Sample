697d716362ef8b7e9c704867e8095d3e023d089f
Bill Ramsour <bill@unity3d.com>
2019-08-12 13:36:02 -0500

    updated to 2019.3 and added an atlas example.

--- Basic/Sprite Land/Assets/Scripts/SpriteControlTest.cs
+++ Basic/Sprite Land/Assets/Scripts/SpriteControlTest.cs
@@ -1,58 +1,62 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
-using UnityEngine.Experimental.UIElements;
 using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.U2D;
+using UnityEngine.UI;
 
 public class SpriteControlTest : MonoBehaviour
 {
     public SpriteRenderer singleSpriteTest;
-    public AssetReferenceSprite swapToSingle;
-    Sprite singleEntry = null;
+    public AssetReferenceSprite singleSpriteReference;
 
     public SpriteRenderer spriteSheetTest;
-    public string spriteSheetAddress;
-    Sprite sheetEntry = null;
+    public AssetReference spriteSheetReference;
 
-    int clickCount = 0;
+    public SpriteRenderer spriteAtlasTest;
+    public AssetReferenceAtlas spriteAtlasReference;
+
+    public Button button;
+    public Text buttonText;
+    
+
+    int m_ClickCount = 0;
     public void OnButtonClick()
     {
         
-        clickCount++;
-        if(clickCount == 1)
-            swapToSingle.LoadAsset().Completed += SingleDone;
-        else if(clickCount == 2)
-            Addressables.LoadAsset<IList<Sprite>>(spriteSheetAddress).Completed += SheetDone;
+        button.interactable = false;
+        m_ClickCount++;
+        switch (m_ClickCount)
+        {
+            case 1:
+                singleSpriteReference.LoadAssetAsync().Completed += SingleDone;
+                break;
+            case 2:
+                spriteSheetReference.LoadAssetAsync<IList<Sprite>>().Completed += SheetDone;
+                break;
+            case 3:
+                spriteAtlasReference.LoadAssetAsync().Completed += AtlasDone;
+                break;
+            case 4:
+                break;
+        }
         
 
     }
-
-    void Update()
-    {
-        if (sheetEntry != null)
-        {
-            spriteSheetTest.sprite = sheetEntry;
-            sheetEntry = null;
-        }
-
-        if (singleEntry != null)
-        {
-            singleSpriteTest.sprite = singleEntry;
-            singleEntry = null;
-        }
-    }
-    
     void SingleDone(AsyncOperationHandle<Sprite> op)
     {
         if (op.Result == null)
         {
-            Debug.LogError("no sheets here.");
+            Debug.LogError("no sprites here.");
             return;
         }
         
-        //saving result, and setting on sprite later to work around engine bug :(
-        singleEntry = op.Result;
+        singleSpriteTest.sprite = op.Result;
+        
+        button.interactable = true;
+        buttonText.text = "Change with sheet list";
     }
 
     void SheetDone(AsyncOperationHandle<IList<Sprite>> op)
@@ -63,7 +67,29 @@ public class SpriteControlTest : MonoBehaviour
             return;
         }
 
-        //saving result, and setting on sprite later to work around engine bug :(
-        sheetEntry = op.Result[3];
+        spriteSheetTest.sprite = op.Result[3];
+        
+        button.interactable = true;
+        buttonText.text = "Change with atlas";
     }
+
+    void AtlasDone(AsyncOperationHandle<SpriteAtlas> op)
+    {
+        if (op.Result == null)
+        {
+            Debug.LogError("no atlases here.");
+            return;
+        }
+
+        spriteAtlasTest.sprite = op.Result.GetSprite("battle_1");
+        
+        buttonText.text = "More options coming with 1.2";
+    }
+
+}
+
+[Serializable]
+public class AssetReferenceAtlas : AssetReferenceT<SpriteAtlas>
+{
+    public AssetReferenceAtlas(string guid) : base(guid) { }
 }
