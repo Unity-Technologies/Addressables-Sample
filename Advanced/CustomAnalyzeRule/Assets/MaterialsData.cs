058c57e42cb46763d2c633c9458290bf3453267a
Kirsten Pilla <kirsten.pilla@unity3d.com>
2022-03-03 16:49:14 -0500

    created test assets for multi-isolation group sample

---
+++ Advanced/CustomAnalyzeRule/Assets/MaterialsData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+[CreateAssetMenu(fileName = "MaterialsData", menuName = "ScriptableObjects/MaterialsData", order = 1)]
+public class MaterialsData : ScriptableObject
+{
+    public List<Material> materials;
+}
