643eead5dd7961cc253522b34bbc8aa05fb3f59e
Kirsten Pilla <kirsten.pilla@unity3d.com>
2022-03-10 09:59:15 -0500

    update CustomAnalyzeRule to 1.19.19 and import sample

---
+++ Advanced/CustomAnalyzeRule/Assets/Samples/Addressables/1.19.19/Custom Analyze Rules/Editor/AddressHasC.cs
@@ -0,0 +1,60 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.AddressableAssets.Build;
+using UnityEditor.AddressableAssets.GUI;
+using UnityEditor.AddressableAssets.Settings;
+using UnityEditor.AddressableAssets.Settings.GroupSchemas;
+using UnityEngine;
+
+/// <summary>
+/// * This is a non-fixable rule (meaning it will not fix itself).
+/// * When run, it checks that all addresses have a capital C in them.  Any that do not are flagged as errors.
+/// * A rule like this would be useful if your studio enforced some sort of naming convention on addresses. (though it would probably be best if it could fix itself)
+/// </summary>
+public class AddressHasC : UnityEditor.AddressableAssets.Build.AnalyzeRules.AnalyzeRule
+{
+    public override bool CanFix
+    {
+        get { return false;}
+        set { }
+    }
+
+    public override string ruleName
+    {
+        get { return "Ensure all addresses have a 'C'"; }
+    }
+
+    public override List<AnalyzeResult> RefreshAnalysis(AddressableAssetSettings settings)
+    {
+        List<AnalyzeResult> results = new List<AnalyzeResult>();
+        foreach (var group in settings.groups)
+        {
+            if (group.HasSchema<PlayerDataGroupSchema>())
+                continue;
+            
+            foreach (var e in group.entries)
+            {
+                if(!e.address.Contains("C"))
+                    results.Add(new AnalyzeResult{resultName = group.Name + kDelimiter + e.address, severity = MessageType.Error});
+            }
+        }
+        
+        if (results.Count == 0)
+            results.Add(new AnalyzeResult{resultName = ruleName + " - No issues found."});
+
+        return results;
+    }
+}
+
+
+[InitializeOnLoad]
+class RegisterAddressHasC
+{
+    static RegisterAddressHasC()
+    {
+        AnalyzeSystem.RegisterNewRule<AddressHasC>();
+    }
+}
+#endif
---
+++ Advanced/CustomAnalyzeRule/Assets/Samples/Addressables/1.19.19/Custom Analyze Rules/Editor/PathAddressIsPath.cs
@@ -0,0 +1,81 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.AddressableAssets.Build;
+using UnityEditor.AddressableAssets.GUI;
+using UnityEditor.AddressableAssets.Settings;
+using UnityEditor.AddressableAssets.Settings.GroupSchemas;
+using UnityEngine;
+
+/// <summary>
+/// * This is a fixable rule.  Running fix on it will change addresses to comply with the rule.
+/// * When run, it first identifies all addresses that seem to be paths.  Of those, it makes sure that the address actually matches the path of the asset.
+/// * This would be useful if you primarily left the addresses of your assets as the path (which is the default when marking an asset addressable).  If the asset is moved within the project, then the address no longer maps to where it is. This rule could fix that.
+/// </summary>
+public class PathAddressIsPath : UnityEditor.AddressableAssets.Build.AnalyzeRules.AnalyzeRule
+{
+    public override bool CanFix
+    {
+        get { return true;}
+        set { }
+    }
+
+    public override string ruleName
+    {
+        get { return "Addresses that are paths, match path"; }
+    }
+
+    [SerializeField]
+    List<AddressableAssetEntry> m_MisnamedEntries = new List<AddressableAssetEntry>();
+
+    public override List<AnalyzeResult> RefreshAnalysis(AddressableAssetSettings settings)
+    {
+        List<AnalyzeResult> results = new List<AnalyzeResult>();
+        foreach (var group in settings.groups)
+        {
+            if (group.HasSchema<PlayerDataGroupSchema>())
+                continue;
+
+            foreach (var e in group.entries)
+            {
+                if (e.address.Contains("Assets") && e.address.Contains("/") && e.address != e.AssetPath)
+                {
+                    m_MisnamedEntries.Add(e);
+                    results.Add(new AnalyzeResult { resultName = group.Name + kDelimiter + e.address, severity = MessageType.Error });
+                }
+            }
+        }
+
+        if (results.Count == 0)
+            results.Add(new AnalyzeResult{resultName = "No issues found."});
+
+    return results;
+    }
+
+    public override void FixIssues(AddressableAssetSettings settings)
+    {
+        foreach (var e in m_MisnamedEntries)
+        {
+            e.address = e.AssetPath;
+        }
+        m_MisnamedEntries = new List<AddressableAssetEntry>();
+    }
+
+    public override void ClearAnalysis()
+    {
+        m_MisnamedEntries = new List<AddressableAssetEntry>();
+    }
+}
+
+
+[InitializeOnLoad]
+class RegisterPathAddressIsPath
+{
+    static RegisterPathAddressIsPath()
+    {
+        AnalyzeSystem.RegisterNewRule<PathAddressIsPath>();
+    }
+}
+#endif
