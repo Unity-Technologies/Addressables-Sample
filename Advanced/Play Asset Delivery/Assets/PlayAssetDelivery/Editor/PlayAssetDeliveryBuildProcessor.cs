d5367a091ae70305faabe499028063d88f90ad9b
Kirsten Pilla <kirsten.pilla@unity3d.com>
2022-03-30 12:10:53 -0400

    add custom BuildPlayerProcessor for 21.2+

--- Advanced/Play Asset Delivery/Assets/PlayAssetDelivery/Editor/PlayAssetDeliveryBuildProcessor.cs
+++ Advanced/Play Asset Delivery/Assets/PlayAssetDelivery/Editor/PlayAssetDeliveryBuildProcessor.cs
@@ -10,6 +10,108 @@ using UnityEngine;
 
 namespace AddressablesPlayAssetDelivery.Editor
 {
+#if UNITY_2021_2_OR_NEWER
+    /// <summary>
+    /// Moves custom asset pack data from their default build location <see cref="BuildScriptPlayAssetDelivery"/> to their correct player build data location.
+    /// For an Android App Bundle, bundles assigned to a custom asset pack must be located in their {asset pack name}.androidpack directory in the Assets folder.
+    /// The 'CustomAssetPacksData.json' file is also added to the built player's StreamingAssets file location.
+    ///
+    /// This script executes before the <see cref="AddressablesPlayerBuildProcessor"/> which moves all Addressables data to StreamingAssets.
+    public class PlayAssetDeliveryBuildProcessor : BuildPlayerProcessor
+    {
+        /// <summary>
+        /// Returns the player build processor callback order.
+        /// </summary>
+        public override int callbackOrder
+        {
+            get { return 0; }
+        }
+
+        /// <summary>
+        /// Invoked before performing a Player build. Maintains building Addressables step and processing Addressables build data.
+        /// </summary>
+        /// <param name="buildPlayerContext">Contains data related to the player.</param>
+        public override void PrepareForBuild(BuildPlayerContext buildPlayerContext)
+        {
+           if (EditorUserBuildSettings.activeBuildTarget == BuildTarget.Android && EditorUserBuildSettings.buildAppBundle)
+                MoveDataForAppBundleBuild(buildPlayerContext);
+            else
+                MoveDataToDefaultLocation();
+        }
+
+        /// <summary>
+        /// Move custom asset pack data from their build location to their App Bundle data location.
+        /// </summary>
+        /// <param name="buildPlayerContext">Contains data related to the player.</param>
+        public static void MoveDataForAppBundleBuild(BuildPlayerContext buildPlayerContext)
+        {
+            try
+            {
+                buildPlayerContext.AddAdditionalPathToStreamingAssets(CustomAssetPackUtility.CustomAssetPacksDataEditorPath, CustomAssetPackUtility.kCustomAssetPackDataFilename);
+                
+                AssetDatabase.StartAssetEditing();
+                if (File.Exists(CustomAssetPackUtility.BuildProcessorDataPath))
+                {
+                    string contents = File.ReadAllText(CustomAssetPackUtility.BuildProcessorDataPath);
+                    var data =  JsonUtility.FromJson<BuildProcessorData>(contents);
+
+                    foreach (BuildProcessorDataEntry entry in data.Entries)
+                    {
+                        string assetsFolderPath = Path.Combine(CustomAssetPackUtility.PackContentRootDirectory, entry.AssetsSubfolderPath);
+                        if (File.Exists(entry.BundleBuildPath))
+                        {
+                            File.Move(entry.BundleBuildPath, assetsFolderPath);
+                            File.Delete(entry.BundleBuildPath + ".meta");
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"Exception occured when moving data for an app bundle build: {e.Message}.");
+            }
+            finally
+            {
+                AssetDatabase.StopAssetEditing();
+            }
+        }
+
+        /// <summary>
+        /// Move custom asset pack data from their App Bundle data location to to their build location.
+        /// </summary>
+        public static void MoveDataToDefaultLocation()
+        {
+            try
+            {
+                AssetDatabase.StartAssetEditing();
+
+                if (File.Exists(CustomAssetPackUtility.BuildProcessorDataPath))
+                {
+                    string contents = File.ReadAllText(CustomAssetPackUtility.BuildProcessorDataPath);
+                    var data =  JsonUtility.FromJson<BuildProcessorData>(contents);
+
+                    foreach (BuildProcessorDataEntry entry in data.Entries)
+                    {
+                        string assetsFolderPath = Path.Combine(CustomAssetPackUtility.PackContentRootDirectory, entry.AssetsSubfolderPath);
+                        if (File.Exists(assetsFolderPath))
+                        {
+                            File.Move(assetsFolderPath, entry.BundleBuildPath);
+                            File.Delete(assetsFolderPath + ".meta");
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"Exception occured when moving data for a player build: {e.Message}.");
+            }
+            finally
+            {
+                AssetDatabase.StopAssetEditing();
+            }
+        }
+    }
+#else
     /// <summary>
     /// Moves custom asset pack data from their default build location <see cref="BuildScriptPlayAssetDelivery"/> to their correct player build data location.
     /// For an Android App Bundle, bundles assigned to a custom asset pack must be located in their {asset pack name}.androidpack directory in the Assets folder.
@@ -121,4 +223,5 @@ namespace AddressablesPlayAssetDelivery.Editor
             }
         }
     }
+#endif
 }
